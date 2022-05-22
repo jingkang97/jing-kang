f6d0c1cba720f8732f83537b78a2d9ae2ab38e0a
jingkang97 <jingkang@lomotif.com>
2022-05-22 16:05:40 +0800

    fix: font weight

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cbf94a240405d55a"; }
+/******/ 		__webpack_require__.h = function() { return "af8a117b82a41b14"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
